3a3b6fcada545ca84677d773ee31653aed123b97
Methusael Murmu <blendit07@gmail.com>
2017-10-16 23:37:30 +0530

    Modified license notice

--- src/gfx/gfx.c
+++ src/gfx/gfx.c
@@ -1,7 +1,11 @@
 /*
 A simple graphics library for CSE 20211 by Douglas Thain
+Modified: Methusael Murmu
+
 For complete documentation, see:
 http://www.nd.edu/~dthain/courses/cse20211/fall2011/gfx
+
+2017 - Added support for non-resizable windows
 Version 3, 11/07/2012 - Now much faster at changing colors rapidly.
 Version 2, 9/23/2011 - Fixes a bug that could result in jerky animation.
 */
